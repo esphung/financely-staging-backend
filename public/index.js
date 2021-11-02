da7117f8c66d0865a79b3258155d27b1ac3a75ab
Eric Phung <esphung@gmail.com>
2021-11-02 15:14:15 -0500

    colors

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 
-router.use(bodyParser.json())
+router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
@@ -10,7 +10,12 @@ router.get('/', (req, res) => {
 
 router.post('/authenticate', (req, res) => {
   console.log('req.body: ', req.body);
-  res.json(req.body);
+  res.json({ success: true, data: { authenticated: true } });
+});
+
+router.post('/token', (req, res) => {
+  const createToken = () => 'token_' + Math.random().toString(36).substr(2, 9);
+  res.json({ success: true, data: { token: createToken() } });
 });
 
 module.exports = router;
