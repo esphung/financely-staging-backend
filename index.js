609ef9167b8951b816ee0e0bd32312d9643ce7df
Eric Phung <esphung@gmail.com>
2021-11-04 04:15:56 -0500

    added sql db

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
-console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+// console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+
+const { NODE_ENV, DB_HOST, PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
+console.log({ NODE_ENV, DB_HOST, PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE });
+
 require('app-module-path').addPath(__dirname);
 
 require('dotenv').config();
--- public/index.js
+++ public/index.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+// router.use(bodyParser.json());
 
-router.use(bodyParser.json());
-
-const knex = require('knex')({
+let config = {
   client: 'mysql',
   connection: {
     host: process.env.DB_HOST, // 127.0.0.1 <= local
-    port: process.env.DB_PORT,
+    port: 3306,
     user: process.env.DB_USERNAME, // <= root
     password: process.env.DB_PASSWORD, // <= password
     database: process.env.DB_DATABASE, // <= mydb
   },
-});
+}
+const knex = require('knex')(config);
 
-const listTable = (table) =>
-  knex(table)
+const listTable = (table) => {
+  return knex(table)
     .orderBy('id', 'desc')
     .then((rows) => rows)
     .catch((err) => err);
+}
 
 router.get('/:table', ({ params }, res) =>
   listTable(params?.table).then((result) => res.jsonp(result)),
