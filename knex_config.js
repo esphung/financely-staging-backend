b18c5b8cec614815fc18eeb4ff70256a4119fa13
Eric Phung <esphung@gmail.com>
2021-11-04 08:26:37 -0500

    added ip

--- knex_config.js
+++ knex_config.js
@@ -1,7 +1,7 @@
 let config = {
   client: 'mysql',
   connection: {
-    host: process.env.RDS_HOSTNAME, // 127.0.0.1 <= local
+    host: process.env.DB_HOST, // 127.0.0.1 <= local
     port: process.env.RDS_PORT,
     user: process.env.RDS_USERNAME, // <= root
     password: process.env.RDS_PASSWORD, // <= password
--- server.js
+++ server.js
@@ -17,46 +17,46 @@ var mysql = require('mysql');
 
 const sql = require('mssql');
 
-const sqlConfig = {
-	user: process.env.RDS_USERNAME,
-	password: process.env.RDS_PASSWORD,
-	database: process.env.RDS_DB_NAME,
-	server: process.env.RDS_HOSTNAME,
-	port: process.env.RDS_PORT,
-	pool: {
-		max: 10,
-		min: 0,
-		idleTimeoutMillis: 30000,
-	},
-	options: {
-		encrypt: true, // for azure
-		trustServerCertificate: false, // change to true for local dev / self-signed certs
-	},
-};
+// const sqlConfig = {
+// 	user: process.env.RDS_USERNAME,
+// 	password: process.env.RDS_PASSWORD,
+// 	database: process.env.RDS_DB_NAME,
+// 	server: process.env.RDS_HOSTNAME,
+// 	port: process.env.RDS_PORT,
+// 	pool: {
+// 		max: 10,
+// 		min: 0,
+// 		idleTimeoutMillis: 30000,
+// 	},
+// 	options: {
+// 		encrypt: true, // for azure
+// 		trustServerCertificate: false, // change to true for local dev / self-signed certs
+// 	},
+// };
 
-server.get('/', async (req, res) => {
-	let result;
-	const loadResource = async () => {
-		try {
-			// make sure that any items are correctly URL encoded in the connection string
-			result = await sql.connect(sqlConfig);
-			const result = await sql.query`select * from mytable where id = ${value}`;
-			console.log({ result });
+// server.get('/', async (req, res) => {
+// 	let result;
+// 	const loadResource = async () => {
+// 		try {
+// 			// make sure that any items are correctly URL encoded in the connection string
+// 			result = await sql.connect(sqlConfig);
+// 			const result = await sql.query`select * from mytable where id = ${value}`;
+// 			console.log({ result });
 
-			res.jsonp({ result });
-		} catch (err) {
-			result = err;
-			console.log('err: ', err);
-			// ... error checks
+// 			res.jsonp({ result });
+// 		} catch (err) {
+// 			result = err;
+// 			console.log('err: ', err);
+// 			// ... error checks
 
-			res.jsonp({ result });
-		}
-	};
+// 			res.jsonp({ result });
+// 		}
+// 	};
 
-	loadResource()
+// 	loadResource()
 
 	
-});
+// });
 
 // var connection = mysql.createConnection({
 // 	host: process.env.RDS_HOSTNAME,
